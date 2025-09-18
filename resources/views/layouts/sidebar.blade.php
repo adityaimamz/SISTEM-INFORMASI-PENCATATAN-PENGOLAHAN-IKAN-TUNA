5e73a79216bbf9e4768563b9a0642b6c64955e94
BallonAmr <93809669+BallonAmr@users.noreply.github.com>
2025-09-18 14:35:00 +0700

    47

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -134,10 +134,6 @@
                             <a href="/cutting" class="submenu-link">Cutting by Produk</a>
                         </li>
 
-                        <li class="submenu-item  {{ Request::is('cuttingl') ? 'active' : '' }}">
-                            <a href="/cuttingl" class="submenu-link">Cutting by Loin</a>
-                        </li>
-
                         <li class="submenu-item {{ Request::is('service') ? 'active' : '' }} ">
                             <a href="/service" class="submenu-link">Service</a>
                         </li>
