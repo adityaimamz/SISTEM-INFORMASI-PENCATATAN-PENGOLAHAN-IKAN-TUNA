7820a67b24bac57cea0a5e6d326676d2292b192c
BallonAmr <93809669+BallonAmr@users.noreply.github.com>
2025-09-22 10:41:08 +0700

    57

--- database/migrations/2025_09_17_085507_create_cuttingls_table.php
+++ database/migrations/2025_09_17_085507_create_cuttingls_table.php
@@ -16,10 +16,20 @@ return new class extends Migration
             $table->date('tggl_cutting');
             $table->date('tggl_injek_co');
             $table->date('tggl_service');
-            $table->unsignedInteger('grade_size_id');
+            $table->unsignedBigInteger('grade_size_id');
+            $table->foreignId('grade_size_id')
+                    ->constrained('grade_sizes')
+                    ->onDelete('cascade');
+            $table->unsignedBigInteger('grade_service_id');
+            $table->foreignId('grade_service_id')
+                    ->constrained('grade_services')
+                    ->onDelete('cascade');
             $table->float('berat_loin');
             $table->float('suhu_loin');
             $table->unsignedBigInteger('penerimaan_id');
+            $table->foreignId('penerimaan_id')
+                    ->constrained('penerimaan_ikans')
+                    ->onDelete('cascade');
             $table->timestamps();
         });
     }
