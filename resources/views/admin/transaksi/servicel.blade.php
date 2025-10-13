dfc081d51115b4145bcf32c5956acfc9b26c72a1
BallonAmr <93809669+BallonAmr@users.noreply.github.com>
2025-10-13 13:47:46 +0700

    103

---
+++ resources/views/admin/transaksi/servicel.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="main">
+    <header class="mb-3">
+        <a href="#" class="burger-btn d-block d-xl-none">
+            <i class="bi bi-justify fs-3"></i>
+        </a>
+    </header>
+
+    <div class="page-heading">
+        <div class="page-title">
+            <div class="row">
+                <div class="col-12 col-md-6 order-md-1 order-last">
+                    <h3>Data Service L</h3>
+                    <p class="text-subtitle text-muted">Silahkan kelola data service Loin</p>
+                </div>
+
+                <div class="col-12 col-md-6 order-md-2 order-first">
+                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">{{ Request::segment(1) }}</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <section class="section">
+            <!-- Modal untuk Tambah Penerimaan (Supplier + Date) -->
+            <div class="modal fade" id="tambahPenerimaanModal" tabindex="-1" role="dialog"
+                aria-labelledby="tambahPenerimaanModalTitle" aria-hidden="true">
+            </div>
+            <div class="card-body">
+                @livewire('servicel')
+            </div>
+        </section>
+    </div>
+</div>
+@endsection
